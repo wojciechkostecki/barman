adf8f37710ca8ee3bd88ffe0e662196a08e8f678
Wojciech Kostecki <wt.kostecki@gmail.com>
2020-03-29 12:40:26 +0200

    Creation method printDrink

--- src/Barman.java
+++ src/Barman.java
@@ -1,7 +1,23 @@
 public class Barman {
+    public static void main(String[] args) {
+    }
 
-    public Drink createDrink(Ingredient ingredient1, Ingredient ingredient2, Ingredient ingredient3){
+    public static Drink createDrink(Ingredient ingredient1, Ingredient ingredient2, Ingredient ingredient3){
         return new Drink(ingredient1,ingredient2,ingredient3);
+    }
+
+    public static void printDrink(Drink drink){
+        double sum = drink.ingredient1.amountOfIngredient + drink.ingredient2.amountOfIngredient +
+                drink.ingredient3.amountOfIngredient;
+
+        double proportion1 = drink.ingredient1.amountOfIngredient/sum;
+        double proportion2 = drink.ingredient2.amountOfIngredient/sum;
+        double proportion3 = drink.ingredient3.amountOfIngredient/sum;
+
+        System.out.println("Składniki drinka to: " + drink.ingredient1.ingredientName + ", " +
+                drink.ingredient2.ingredientName + ", " + drink.ingredient3.ingredientName +
+                " w proporcjach " + proportion1 + ", " + proportion2 + ", " + proportion3 + "." +
+                " Jego pojemność to " + sum + "ml.");
     }
 
 }
--- src/Drink.java
+++ src/Drink.java
@@ -8,4 +8,28 @@ public class Drink {
         this.ingredient2 = ingredient2;
         this.ingredient3 = ingredient3;
     }
+
+    public Ingredient getIngredient1() {
+        return ingredient1;
+    }
+
+    public void setIngredient1(Ingredient ingredient1) {
+        this.ingredient1 = ingredient1;
+    }
+
+    public Ingredient getIngredient2() {
+        return ingredient2;
+    }
+
+    public void setIngredient2(Ingredient ingredient2) {
+        this.ingredient2 = ingredient2;
+    }
+
+    public Ingredient getIngredient3() {
+        return ingredient3;
+    }
+
+    public void setIngredient3(Ingredient ingredient3) {
+        this.ingredient3 = ingredient3;
+    }
 }
